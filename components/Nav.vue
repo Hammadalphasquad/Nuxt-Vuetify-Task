f8ce5508927c984bdbfd87ace8bf1765a02b042a
Hammadalphasquad <hammad.alphasquad@gmail.com>
2023-10-10 05:09:32 +0500

    done

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="pl-10 py-6 border-[#1A202C] border-b-2">
+<div class="pl-10 py-6 border-[e5e7eb] border-b-2">
       <button
         class="text-base font-bold text-white cursor-pointer px-10 py-3 rounded-tl-[11px] rounded-tr-[20px] rounded-br-[6px] rounded-bl-[33px] bg-[#2D3648]"
       >
